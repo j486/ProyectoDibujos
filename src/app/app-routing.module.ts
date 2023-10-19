eb0df982a561171cad7ef0093ca925fdeabaf3e7
Jose Antonio <japgrguez@gmail.com>
2023-10-19 20:01:45 +0200

    Subida Cambos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { DibujosComponent } from './componentes/dibujos/dibujos.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '',
+      component: InicioComponent,
+      pathMatch: 'full'
+  },
+  {
+    path: 'dibujos',
+    component: DibujosComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'acerca-de',
+    component: AcercaDeComponent,
+    pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: ''
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { DibujosComponent } from './componentes/dibujos/dibujos.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { CarouselComponent } from './componentes/carousel/carousel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MostrarImagenesComponent } from './componentes/mostrar-imagenes/mostrar-imagenes.component';
+import { ModalVerDibujosComponent } from './componentes/modal-ver-dibujos/modal-ver-dibujos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    InicioComponent,
+    DibujosComponent,
+    AcercaDeComponent,
+    CarouselComponent,
+    MostrarImagenesComponent,
+    ModalVerDibujosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
